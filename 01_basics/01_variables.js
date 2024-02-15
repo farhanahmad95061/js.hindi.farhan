172f0d492c33ff167a93035d809a1504b44903ab
farhanahmad95061 <160132004+farhanahmad95061@users.noreply.github.com>
2024-02-15 14:04:19 +0000

    done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountid=1444
+let accountdetail="farhan"
+var accountcity="jaipur"
+username= "farhan ahmad"
+ //u cannot cahnge the const
+ //accountid=2 not allowed
+ //var is no longer used
+console.log(accountid)
+console.table([accountid,accountdetail,accountcity,username])
---
+++ 01_basics/datatype.js
@@ -0,0 +1,8 @@
+// number>> 2 to power 53
+//bigint
+//string=>""
+//boolean=>true/false
+//null=> stand alone value
+//undefined
+// symbol=> unique
+//object
