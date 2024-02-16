65949337ada57007537c5c5a6745a0b559a9e33e
farhanahmad95061 <160132004+farhanahmad95061@users.noreply.github.com>
2024-02-16 07:09:43 +0000

    datatype done

---
+++ 01_basics/datatypeconversion.js
@@ -0,0 +1,10 @@
+let score=3
+let string="3"
+console.log(typeof score )
+console.log(typeof string )
+ let valueofscore=String(score)
+ let valueofstring=Number(string)
+ console.log( typeof valueofscore)
+ console.log(typeof valueofstring )
+// if boolean ,,1=true,0=false,""=false,"farhan"=true;
+//
