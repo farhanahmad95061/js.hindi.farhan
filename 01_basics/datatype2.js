ca99ce70de7624e9e5e435604b59a5a6e4c257c6
farhanahmad95061 <160132004+farhanahmad95061@users.noreply.github.com>
2024-02-16 20:28:38 +0000

    done

---
+++ 01_basics/datatype2.js
@@ -0,0 +1,14 @@
+let char =Symbol('123')
+let num =Symbol('123')
+console.log(char===num)
+//array
+const heroes=["shaktiman","krish","flying jatt"]
+//object 
+let myobj={
+    name:"farhan",
+    age:21,
+}
+
+const myfunction=function(){
+    console.log("hello bsdko");
+}
